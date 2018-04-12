bebea3bb42904127b4fce08e2c45e2eb8515c54b
suleyman.yildirim <suleyman.yildirim@CPX-IOTTIBSY5PL>
2018-04-12 22:55:35 +0100

    Given an array A consisting of N integers, returns the value of the unpaired element.

---
+++ src/arrays/oldoccurences/Solution.java
@@ -0,0 +1,56 @@
+package arrays.oldoccurences;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * 
+ * @author suleyman.yildirim
+ *
+ */
+public class Solution {
+
+	public int solution(int[] A) {
+
+		int solution = -1;
+		Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+
+		//create map from array
+		for (int i = 0; i < A.length; i++) {
+			if (!map.containsKey(A[i])) {
+				ArrayList<Integer> list = new ArrayList<Integer>();
+				list.add(i);
+				map.put(A[i], list);
+			} else {
+				ArrayList<Integer> list2 = map.get(A[i]);
+				list2.add(i);
+				map.put(A[i], list2);
+			}
+		}
+		
+		//find unpaired element
+		for (Entry<Integer, ArrayList<Integer>> entry : map.entrySet()) {
+			if (entry.getValue().size() == 1) {
+				solution = entry.getKey();
+				break;
+			}
+		}
+
+		System.out.println(map.toString());
+		System.out.println(solution);
+		return solution;
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] A = new int[] { 9, 3, 9, 3, 9, 7, 9 };
+		Solution s = new Solution();
+		s.solution(A);
+
+	}
+
+}
