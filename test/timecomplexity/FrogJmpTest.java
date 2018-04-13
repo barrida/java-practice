f0f45fdfe58847b9248dd7111655ab5224dc62c5
suleyman.yildirim <suleyman.yildirim@CPX-IOTTIBSY5PL.dir.svc.accenture.com>
2018-04-13 19:20:48 +0100

    Frog Jump: first failed unit test and implementation

---
+++ test/timecomplexity/FrogJmpTest.java
@@ -0,0 +1,14 @@
+package timecomplexity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FrogJmpTest {
+
+	@Test
+	public void testSolution() {
+		fail("Not yet implemented");
+	}
+
+}
