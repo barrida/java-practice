9c01e2e3abc72517e0ff1bd852edb0e9b667ec7c
suleyman.yildirim <suleyman.yildirim@CPX-IOTTIBSY5PL.dir.svc.accenture.com>
2018-09-08 23:09:38 +0100

    permutation

---
+++ src/lesson_4_counting_elements/PermCheck.java
@@ -0,0 +1,38 @@
+package lesson_4_counting_elements;
+
+/**
+ * Checks if an array is a permutation.
+ * 
+ * @author suleyman.yildirim
+ *
+ */
+public class PermCheck {
+
+	/**
+	 * create array Count
+	 * For each element i in A
+	 * 		increase the counter for A[i]
+	 * if 
+	 * 
+	 * @param A
+	 * @return
+	 */
+	public int solution(int[] A){
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PermCheck permcheck = new PermCheck();
+		
+		int[] A = new int[] {1,3,5,2};
+		if (permcheck.solution(A) == 1) {
+			System.out.println("Permutation");
+		} else {
+			System.out.println("Not Permutation");
+		}
+	}
+
+}
