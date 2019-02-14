a772ea7407a1454e83673e7d9950924ed3f402bf
Yildirim, Suleyman <suleyman.yildirim@accenture.com>
2019-02-14 16:18:28 +0000

    comments

--- src/coursera/week1/MaxPairwiseProduct.java
+++ src/coursera/week1/MaxPairwiseProduct.java
@@ -6,23 +6,18 @@ import java.math.BigInteger;
 
 public class MaxPairwiseProduct {
 	static BigInteger getMaxPairwiseProduct(int[] numbers) {
-
-		
-		// int n = numbers.length;
+	
 		BigInteger n = BigInteger.valueOf(numbers.length);
-
+	
+		//find first max value
 		int maxIndex1 = -1;
-		// for (int first = 0; first < n; first++) {
-		// if ((maxIndex1 == -1) || (numbers[first] > numbers[maxIndex1]))
-		// maxIndex1 = first;
-		// }
-
 		for (BigInteger first = BigInteger.valueOf(0); first.compareTo(n) < 0; first = first.add(BigInteger.ONE)) {
 			int value = (int) first.intValue();
 			if ((maxIndex1 == -1) || (numbers[value] > numbers[maxIndex1]))
 				maxIndex1 = value;
 		}
 
+		//find second max value
 		int maxIndex2 = -1;
 		for (BigInteger second = BigInteger.valueOf(0); second.compareTo(n) < 0; second = second.add(BigInteger.ONE)) {
 			int value = (int) second.intValue();
